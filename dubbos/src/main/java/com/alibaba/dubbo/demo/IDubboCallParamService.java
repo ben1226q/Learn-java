4602b3a158cccd4f8f24971211b4ee0bd70e0f89
ben1226q <249893628@qq.com>
2018-11-26 20:56:46 +0800

    commit

--- dubbos/src/main/java/com/alibaba/dubbo/demo/IDubboCallParamService.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.dubbo.demo;
-
-import com.alibaba.dubbo.demo.entity.Computer;
-import com.alibaba.dubbo.demo.provider.CallbackListener;
-
-public interface IDubboCallParamService {
-    public Computer methodInvoke(String value, CallbackListener callbackListener);
-}
--- web1/src/main/webapp/WEB-INF/views/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello views!</h2>
-</body>
-</html>
